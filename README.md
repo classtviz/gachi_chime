051489e2328f0dff17f1d1fa3c4b2a733f7dffa2
yanina <yaninagusak@gmail.com>
2021-02-02 20:55:18 +0300

    first and hopefully last

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gachi_chime
+Lovely sounds for connoisseurs.
+Instructions are the [are the same](https://github.com/MaxHalford/chime#basic-usage).
---
+++ gachichime.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install chime
+pip install gdown
+
+theme_path=$(python -c "import chime; print('/'.join(str(chime.current_theme_dir()).split('/')[:-1]))")
+theme_path=$theme_path"/gachi"
+mkdir "$theme_path"
+cd /tmp
+mkdir chimes
+cd chimes
+gdown https://drive.google.com/uc?id=1uo4ObQuPfZsk68b1980j0VdAlhROk5Yd
+tar -xf gachi_wavs.tar.xz
+cp *.wav $theme_path
+echo "$(ls $theme_path)"
